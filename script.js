0f38fd35db956d94b89d8836ea7d893a03f5389a
Aanand Kumar <aanandmrh222@gmail.com>
2024-07-13 18:09:57 +0530

    commit

---
+++ script.js
@@ -0,0 +1,49 @@
+//  a smooth scroll function
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    const changingText = document.getElementById('changing-text');
+    const texts = [
+        "Student",
+        "Software Developer",
+        "UI UX Designer"
+    ];
+    let index = 0;
+    let textIndex = 0;
+    let isDeleting = false;
+
+    function type() {
+        const currentText = texts[index];
+        if (isDeleting) {
+            changingText.textContent = currentText.substring(0, textIndex);
+            textIndex--;
+            if (textIndex < 0) {
+                isDeleting = false;
+                index = (index + 1) % texts.length;
+                setTimeout(type, 500); // Short pause before starting to type the next text
+                return;
+            }
+        } else {
+            textIndex++;
+            changingText.textContent = currentText.substring(0, textIndex);
+            if (textIndex === currentText.length) {
+                isDeleting = true;
+                setTimeout(type, 2000); // Wait for 2 seconds before starting to delete
+                return;
+            }
+        }
+
+        setTimeout(type, isDeleting ? 100 : 200);
+    }
+
+    type();
+});
+
