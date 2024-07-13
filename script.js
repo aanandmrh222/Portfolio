b125ea200fd106d4061e3a64b4d9ade7e44ccb12
Aanand Kumar <aanandmrh222@gmail.com>
2024-07-13 22:31:30 +0530

    commit

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
-//  a smooth scroll function
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(e) {
-        e.preventDefault();
-
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
 document.addEventListener('DOMContentLoaded', () => {
     const changingText = document.getElementById('changing-text');
     const texts = [
         "Software Developer",
-        "UI UX Designer"
+        "UI/UX Designer"
     ];
     let index = 0;
     let textIndex = 0;
@@ -46,3 +35,20 @@ document.addEventListener('DOMContentLoaded', () => {
     type();
 });
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+window.addEventListener('scroll', () => {
+    const header = document.querySelector('header');
+    if (window.scrollY > 50) {
+        header.style.backgroundColor = '#222';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+});
